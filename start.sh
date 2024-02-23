8e64250775eff0b4ae78822ce61d496537bb6331
Atikullah WD <156679363+mratikullahwd@users.noreply.github.com>
2024-02-23 16:13:13 +0600

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Fetch user information from GitHub Gist
+USER_INFO=$(curl -sL https://gist.githubusercontent.com/your_username/your_gist_id/raw)
+
+# Parse user information
+USERNAME=$(echo "$USER_INFO" | grep "Username" | cut -d ":" -f 2)
+PASSWORD=$(echo "$USER_INFO" | grep "Password" | cut -d ":" -f 2)
+FULLNAME=$(echo "$USER_INFO" | grep "Fullname" | cut -d ":" -f 2)
+GROUP=$(echo "$USER_INFO" | grep "Group" | cut -d ":" -f 2)
+HOMEDIR=$(echo "$USER_INFO" | grep "Homedir" | cut -d ":" -f 2)
+
+# Function to create user
+create_user() {
+    sudo useradd -m -G $GROUP -s /bin/bash -c "$FULLNAME" $USERNAME
+    echo "$USERNAME:$PASSWORD" | sudo chpasswd
+    sudo chown -R $USERNAME:$USERNAME $HOMEDIR
+    sudo chmod -R 755 $HOMEDIR
+    echo "User $USERNAME created successfully."
+}
+
+# Main function
+main() {
+    echo "Creating user..."
+    create_user
+}
+
+# Execute main function
+main
